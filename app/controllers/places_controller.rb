0fbb3c91960f1b8850cbe37071d231d6c1fa5bb9
alperenbozkurt <bozkurttalperen@gmail.com>
2016-08-12 23:20:38 +0300

    Created Edit Page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,4 +5,27 @@ class PlacesController < ApplicationController
 	def show
 		@place = Place.find(params[:id])
 	end
+	def new
+		@place = Place.new
+	end
+	def create
+		@place = Place.new(strong_params)
+
+		if @place.save
+			redirect_to place_path(@place)
+		else
+			render :new
+		end
+	end
+	def strong_params
+		params.permit(:name, :address, :established_at, :phone_number, :contract_mail, :city, :description)
+	end
+	def edit
+		@place = Place.find(params[:id])
+	end
+	def update
+		Place.update(params[:id], :name => params[:name])
+		@place = Place.find(params[:id])
+		redirect_to place_path(@place)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get '/', to: 'places#index'
-  get 'show/:id', to: 'places#show' 
+  root "places#index"
+  get 'places/new', to: 'places#new'
+  get 'places', to: 'places#index'
+  get 'places/:id', to: 'places#show', as: "place"
+  post 'places', to: 'places#create'
+  get 'places/:id/edit', to: 'places#edit'
+  post 'places/:id', to: 'places#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
